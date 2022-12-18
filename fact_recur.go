3df3eaf2836ef266b8c17da3551b159d0e23face
Richard Hart Woolley <rick.woolley@gmail.com>
2022-12-18 13:09:57 -0800

    Add files via upload

---
+++ fact_recur.go
@@ -0,0 +1,17 @@
+package main
+import ("fmt")
+
+func factorial_recursion(x float64) (y float64) {
+     if x > 0 {
+                    fmt.Println("x is", x, "y is", y, "from top of if")
+          y = x * factorial_recursion(x-1)
+                    fmt.Println("x is", x, "y is", y, "from after recurse")
+     } else {
+          y = 5
+       }
+   return
+}
+
+func main() {
+     fmt.Println(factorial_recursion(4))
+}
