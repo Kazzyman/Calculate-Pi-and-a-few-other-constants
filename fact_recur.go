e8935ce690c145ec675c259ebb7e973350e39910
Richard Hart Woolley <rick.woolley@gmail.com>
2023-05-02 13:37:45 -0700

    Update fact_recur.go

--- fact_recur.go
+++ fact_recur.go
@@ -4,7 +4,7 @@ import ("fmt")
 func factorial_recursion(x float64) (y float64) {
      if x > 0 {
                     fmt.Println("x is", x, "y is", y, "from top of if")
-          y = x * factorial_recursion(x-1)
+          y = x * factorial_recursion(x-1) // calling a func within a func (recursion) 
                     fmt.Println("x is", x, "y is", y, "from after recurse")
      } else {
           y = 5
