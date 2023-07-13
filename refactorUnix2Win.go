0e385f0d3f75228aec6b5f7ee2ed6ddcb5d961f4
Richard Hart Woolley <rick.woolley@gmail.com>
2023-07-13 14:04:17 -0700

    Add files via upload

--- refactorUnix2Win.go
+++ refactorUnix2Win.go
@@ -1,16 +1,21 @@
 package main
 
+// refactorUnix2Win.go
+
 import (
 	"os"
     "fmt"       // Used for printing etc.
-    //"regexp"
+    "regexp"
     "bufio"     // used to count the lines in this file
-    //"log"          
-    "strings"    
 )
 
-var nameOfOldSourceCodeFile = "testFileWin.go"  
-var nameOfNewSourceCodeFile = "targetFileUnix.go"
+var nameOfOldSourceCodeFile = "Unix-0713-0446-v2-dev-2023.go"  
+var nameOfNewSourceCodeFile = "targetFileWindows.go"
+
+var countOfWindowsStart = 0 
+var countOfWindowsEnd = 0 
+var countOfUnixStart = 0 
+var countOfUnixEnd = 0 
 
 func main() {
 
@@ -21,11 +26,16 @@ func main() {
     defer fileHandle.Close()
         scanner := bufio.NewScanner(fileHandle)
         for scanner.Scan() {
-            lineRead := strings.TrimSpace(scanner.Text())   // read a line from the file into lineRead
+            //lineRead := strings.TrimSpace(scanner.Text())   // read a line from the file into lineRead
+            lineRead := scanner.Text() // this fixes my main problem re formatting getting wasted 
 
+					regexWindowsStart 		:= regexp.MustCompile(`\s*\/\*\s*Windows\s*variant`) 			// ... /* Windows variant
+					regexWindowsEnd			:= regexp.MustCompile(`\s*Windows\s*variant\s*\*\/`)			// ... Windows variant */
+					regexUnixStart			:= regexp.MustCompile(`\s*\/\/\s*\/\*\s*Unix\s*variant`)	 	// ... // /* Unix variant
+					regexUnixEnd			:= regexp.MustCompile(`\s*\/\/\s*Unix\s*variant\s*\*\/`)		// ... // Unix variant */  
 
             // write the read line unchanged to the output file if any of these four strings are not found in the read line from the input file
-            if lineRead != `/* Windows variant` && lineRead != `Windows variant */` && lineRead != `// /* Unix variant` && lineRead != `// Unix variant */` {
+   			if !regexWindowsStart.MatchString(lineRead) && !regexWindowsEnd.MatchString(lineRead) && !regexUnixStart.MatchString(lineRead) && !regexUnixEnd.MatchString(lineRead) {
 	            OutputFileHandle, err2 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err2)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
@@ -38,37 +48,45 @@ func main() {
 
 
             // if they ARE found, replace them with the following edited lines 
-            if lineRead == `/* Windows variant` {
+            //if lineRead == `(\s*\/\/\s*\/\*\s*Windows\s*variant)` {
+            if regexWindowsStart.MatchString(lineRead) {
+            	countOfWindowsStart++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "// /* Windows variant\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            /* Windows variant\n")
 	            	check(err5)
             } 
-            if lineRead == `Windows variant */` {
+            //if lineRead == `(\s*\/\/\s*\s*Windows\s*variant)` {
+            if regexWindowsEnd.MatchString(lineRead) {
+            	countOfWindowsEnd++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "// Windows variant */\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            Windows variant */\n")
 	            	check(err5)
             } 
 
-            if lineRead == `// /* Unix variant` {
+            //if lineRead == `(\s*\/\*\s*Unix\s*variant)` {
+            if regexUnixStart.MatchString(lineRead) {
+            	countOfUnixStart++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "/* Unix variant\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            // /* Unix variant\n")
 	            	check(err5)
             } 
-            if lineRead == `// Unix variant */` {
+            //if lineRead == `(\s*Unix\s*variant\s*\*\/)` {
+            if regexUnixEnd.MatchString(lineRead) {
+            	countOfUnixEnd++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "Unix variant */\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            // Unix variant */\n")
 	            	check(err5)
             } 
 
@@ -77,6 +95,7 @@ func main() {
     if scanner.Err() != nil {
         fmt.Println("there was an error scanning")
     }
+    fmt.Println("Refactoring complete, winS:", countOfWindowsStart, "winE:", countOfWindowsEnd, "UnixS:", countOfUnixStart, "UnixE:", countOfUnixEnd)
 }
 
 func check(e error) {   // create a func named check which takes one parameter "e" of type error 
--- refactorWIn2Unix.go
+++ refactorWIn2Unix.go
@@ -1,5 +1,7 @@
 package main
 
+// refactorWin2Unix.go
+
 import (
 	"os"
     "fmt"       // Used for printing etc.
@@ -7,8 +9,13 @@ import (
     "bufio"     // used to count the lines in this file
 )
 
-var nameOfOldSourceCodeFile = "testFileWin.go"  
+var nameOfOldSourceCodeFile = "win-0713-0524-v2-dev-2023.go"  
 var nameOfNewSourceCodeFile = "targetFileUnix.go"
+
+var countOfWindowsStart = 0 
+var countOfWindowsEnd = 0 
+var countOfUnixStart = 0 
+var countOfUnixEnd = 0 
 
 func main() {
 
@@ -25,11 +32,10 @@ func main() {
 					regexWindowsStart 		:= regexp.MustCompile(`\s*\/\/\s*\/\*\s*Windows\s*variant`) 	// ... // /* Windows variant
 					regexWindowsEnd			:= regexp.MustCompile(`\s*\/\/\s*Windows\s*variant\s*\*\/`)		// ... // Windows variant */
 					regexUnixStart			:= regexp.MustCompile(`\s*\/\*\s*Unix\s*variant`)				// ... /* Unix variant
-					regexUnixEnd			:= regexp.MustCompile(`\s*Unix\s*variant\s*\*\/`)				// ... Unix variant */    // I swear to fucking God these are all perfect!!!!!
+					regexUnixEnd			:= regexp.MustCompile(`\s*Unix\s*variant\s*\*\/`)				// ... Unix variant */  
 
             // write the read line unchanged to the output file if any of these four strings are not found in the read line from the input file
    			if !regexWindowsStart.MatchString(lineRead) && !regexWindowsEnd.MatchString(lineRead) && !regexUnixStart.MatchString(lineRead) && !regexUnixEnd.MatchString(lineRead) {
-
 	            OutputFileHandle, err2 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err2)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
@@ -44,42 +50,43 @@ func main() {
             // if they ARE found, replace them with the following edited lines 
             //if lineRead == `(\s*\/\/\s*\/\*\s*Windows\s*variant)` {
             if regexWindowsStart.MatchString(lineRead) {
+            	countOfWindowsStart++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "/* Windows variant\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            /* Windows variant\n")
 	            	check(err5)
             } 
             //if lineRead == `(\s*\/\/\s*\s*Windows\s*variant)` {
             if regexWindowsEnd.MatchString(lineRead) {
-
+            	countOfWindowsEnd++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "Windows variant */\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            Windows variant */\n")
 	            	check(err5)
             } 
 
             //if lineRead == `(\s*\/\*\s*Unix\s*variant)` {
             if regexUnixStart.MatchString(lineRead) {
-
+            	countOfUnixStart++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "// /* Unix variant\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            // /* Unix variant\n")
 	            	check(err5)
             } 
             //if lineRead == `(\s*Unix\s*variant\s*\*\/)` {
             if regexUnixEnd.MatchString(lineRead) {
-
+            	countOfUnixEnd++
                 // write the edited line to the output file
 	            OutputFileHandle, err4 := os.OpenFile(nameOfNewSourceCodeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600) // write to file 
 	            check(err4)                               // ... gets a file handle for output 
 	            defer OutputFileHandle.Close()          // It’s idiomatic to defer a Close immediately after opening a file.
-	        		_, err5 := fmt.Fprint(OutputFileHandle, "// Unix variant */\n")
+	        		_, err5 := fmt.Fprint(OutputFileHandle, "            // Unix variant */\n")
 	            	check(err5)
             } 
 
@@ -88,7 +95,9 @@ func main() {
     if scanner.Err() != nil {
         fmt.Println("there was an error scanning")
     }
+    fmt.Println("Refactoring complete, winS:", countOfWindowsStart, "winE:", countOfWindowsEnd, "UnixS:", countOfUnixStart, "UnixE:", countOfUnixEnd)
 }
+
 
 func check(e error) {   // create a func named check which takes one parameter "e" of type error 
     if e != nil {
