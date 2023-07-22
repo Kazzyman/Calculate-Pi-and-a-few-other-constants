79f30c548e27262adcb1b85c2204ea5c2c0f5c2f
Richard Hart Woolley <rick.woolley@gmail.com>
2023-07-21 17:14:39 -0700

    Add files via upload

--- katakana.go
+++ katakana.go
@@ -16,74 +16,74 @@ func main() {
 
 // 2 lines of the "vowels" (inclusive)
 //  a * 2 :                i * 1       u         e         o * 2 :
-	"aア, あ", "aア, あ",    "iイ, い",    "uウ, う", "eエ, え", "oオ, お",     "uウ, う", "eエ, え", "oオ, お",      
+	"ア, あ", "ア, あ",    "イ, い",    "ウ, う", "エ, え", "オ, お",     "ウ, う", "エ, え", "オ, お",      
 
-// 6 lines of the ka group: (inclusive)
+// 6 lines of the ka group: (inclusive) キ　
 //   ka         ki         ku        ke         ko * 1       /  ga         gi         gu        ge         go * 1          //  naked / ゛
-	"kaカ, か", "kiキ, き", "kuク, く", "keケ, け", "koコ, こ",       "gaガ, が", "giヂ, ぎ", "guグ, ぐ", "geゲ, げ", "goゴ, ご",   //-* ???????????????????????????????????? *-//
+	"カ, か", "ギ, き", "ク, く", "ケ, け", "コ, こ",       "ガ, が", "ギ, ぎ", "グ, ぐ", "ゲ, げ", "ゴ, ご",   //-* ???????????????????????????????????? *-//
 //               ^----^--v                                                 ^^----^
-	           "kiyaキャ, きゃ",  "kiyuキュ, きゅ",  "kiyoキョ, きょ",  // * 1      ^^----^-v                                                // ki, ya yu yo
-	                                                                      "giyaギャ, ぎゃ",  "giyuギュ, ぎゅ",  "giyoギョ, ぎょ",     // * 1 // ki゛, ya yu yo
+	           "キャ, きゃ",  "キュ, きゅ",  "キョ, きょ",  // * 1      ^^----^-v                                                // ki, ya yu yo
+	                                                                      "ギャ, ぎゃ",  "ギュ, ぎゅ",  "ギョ, ぎょ",     // * 1 // ki゛, ya yu yo
 //                       ^               ^               ^                          ^               ^               ^        
 
 // 6 lines of the sa group: (inclusive)
 //   sa         shi         su        se         so * 1      /   za         gi         zu         ze        zo * 1         //  naked / ゛ 
-	"saサ, さ", "shiシ, し", "suス, す", "seセ, せ", "soソ, そ",      "zaザ, ざ", "giジ, じ", "zuズ, ず", "zeゼ, ぜ", "zoゾ, ぞ",  //-* One key "zu", has two values づ and ず *-//
+	"サ, さ", "シ, し", "ス, す", "セ, せ", "ソ, そ",      "ザ, ざ", "ジ, じ", "ズ, ず", "ゼ, ぜ", "ゾ, ぞ",  //-* One key "zu", has two values づ and ず *-//
 //               ^----^---v                                                 ^^----^
-	           "shiyaシャ, しゃ",  "shiyuシュ, しゅ",  "shiyoショ, しょ",  // * 1    ^^----^-v                                               // shi, ya yu yo
-	                                                                       "giyaジャ, じゃ",   "giyuジュ, じゅ",   "giyoジョ, じょ",  // * 1 // shi゛, ya yu yo
+	           "シャ, しゃ",  "シュ, しゅ",  "ショ, しょ",  // * 1    ^^----^-v                                               // shi, ya yu yo
+	                                                                       "ジャ, じゃ",   "ジュ, じゅ",   "ジョ, じょ",  // * 1 // shi゛, ya yu yo
 //                        ^                ^                ^                        ^                ^                ^    
 
 // 6 lines of the ta group: (inclusive)
 //   ta         chi        tsu         te         to * 1     /   da         gi         zu         de         do * 1        //  naked / ゛
-	"taタ, た", "chiチ, ち", "tsuツ, つ", "teテ, て", "toト, と",     "daダ, だ",  "giザ, ぢ", "zuヅ, づ", "deデ, で", "doド, ど",  //-* One key "zu", has two values づ and ず *-//
+	"タ, た", "チ, ち", "ツ, つ", "テ, て", "ト, と",     "ダ, だ",  "ザ, ぢ", "ヅ, づ", "デ, で", "ド, ど",  //-* One key "zu", has two values づ and ず *-//
 //               ^----^---v                                                 ^^----^
-	           "chiyaチャ, ちゃ", "chiyuチュ, ちゅ", "chiyoチョ, ちょ",   // * 1     ^^----^-v                                               // chi, ya yu yo
-	                                                                       "giyaヂャ, ぢゃ",  "giyuヂュ, ぢゅ",  "giyoヂョ, ぢょ",    // * 1 // chi゛, ya yu yo
+	           "チャ, ちゃ", "チュ, ちゅ", "チョ, ちょ",   // * 1     ^^----^-v                                               // chi, ya yu yo
+	                                                                       "ヂャ, ぢゃ",  "ヂュ, ぢゅ",  "ヂョ, ぢょ",    // * 1 // chi゛, ya yu yo
 //                        ^               ^               ^                          ^               ^               ^
 
 // 5 lines of the na group: (inclusive) 'cause they are special -- they like to go naked 
 //   na         ni         nu        ne         no * 1                                                                     //  naked, all of them (there are no ゛s on na's)
-	"naナ, な", "niニ, に", "nuヌ, ぬ", "neネ, ね", "noノ, の",    
+	"ナ, な", "ニ, に", "ヌ, ぬ", "ネ, ね", "ノ, の",    
 //               ^---^---v                                
-               "niyaニャ, にゃ", "niyuニュ, にゅ", "niyoニョ, にょ",   "niyaニャ, にゃ", "niyuニュ, にゅ", "niyoニョ, にょ",                   // * 2 // ni, ya yu yo
+               "ニャ, にゃ", "ニュ, にゅ", "ニョ, にょ",   "ニャ, にゃ", "ニュ, にゅ", "ニョ, にょ",                   // * 2 // ni, ya yu yo
 //                       ^              ^              ^                ^               ^              ^ 
 
 // 9 lines of the ha group: (inclusive) -- the fully-clothed group 
 //  ha         hi         hu * 1     he        ho          he        ho * 2      /  ba         bi         bu         be         bo             //  naked / ゛
-   "haハ, は", "hiヒ, ひ", "huフ, ふ", "heヘ, へ", "hoホ, ほ", "heヘ, へ", "hoホ, ほ",     "baバ, ば", "biビ, び", "buブ, ぶ", "beベ, べ", "boボ, ぼ",  //-* ????? *-// 
+   "ハ, は", "ヒ, ひ", "フ, ふ", "ヘ, へ", "ホ, ほ", "ヘ, へ", "ホ, ほ",     "バ, ば", "ビ, び", "ブ, ぶ", "ベ, べ", "ボ, ぼ",  //-* ????? *-// 
 //              ^---^--v                                                             pa         pi         pu        pe         po            //  poo゜ marks (drag)
-                                                                                    "paパ, ぱ", "piピ, ぴ", "puプ, ぷ", "peペ, ぺ", "poポ, ぽ", //-* ????? *-//
+                                                                                    "パ, ぱ", "ピ, ぴ", "プ, ぷ", "ペ, ぺ", "ポ, ぽ", //-* ????? *-//
 //              ^------v
-	          "hiyaヒャ, ひゃ", "hiyuヒュ, ひゅ", "hiyoヒョ, ひょ",                                                                         // hi, ya yu yo
-	                                                                                "biyaビャ, びゃ", "biyuビュ, びゅ", "biyoビョ, びょ",   // hi゛, ya yu yo (bi)
-	                                                                                "piyaピャ, ぴゃ", "piyuビュ, ぴゅ", "piyoピョ, ぴょ",   // hi゜, ya yu yo (pi)
+	          "ヒャ, ひゃ", "ヒュ, ひゅ", "ヒョ, ひょ",                                                                         // hi, ya yu yo
+	                                                                                "ビャ, びゃ", "ビュ, びゅ", "ビョ, びょ",   // hi゛, ya yu yo (bi)
+	                                                                                "ピャ, ぴゃ", "ビュ, ぴゅ", "ピョ, ぴょ",   // hi゜,  yu yo (pi)
 //                     ^               ^              ^                                       ^              ^              ^   
 
 // 5 lines of the ma group: (inclusive) 'cause they too are special -- and also like to go naked
 //  ma         mi        mu         me         mo * 1                                                                     //  naked, all of them (there are no ゛s on ma's)
-   "maマ, ま", "miミ, み", "muム, む", "meメ, め", "moモ, も",     
+   "マ, ま", "ミ, み", "ム, む", "メ, め", "モ, も",     
 //              ^--^-v
-              "miyaミャ, みゃ", "miyuミュ, みゅ", "miyoミョ, みょ",     "miyaミャ, みゃ", "miyuミュ, みゅ", "miyoミョ, みょ",                  // * 2 // mi, ya yu yo 
+              "ミャ, みゃ", "ミュ, みゅ", "ミョ, みょ",     "ミャ, みゃ", "ミュ, みゅ", "ミョ, みょ",                  // * 2 // mi, ya yu yo 
 //                      ^              ^              ^                  ^              ^              ^
 
 // 3 lines of the ya, yu, yo set: (inclusive)
 //  ya         yu         yo * 3
-   "yaヤ, や", "yuユ, ゆ", "yoヨ, よ","yaヤ, や", "yuユ, ゆ", "yoヨ, よ", "yaヤ, や", "yuユ, ゆ", "yoヨ, よ",    // * 3 (also used as suffixes)
+   "ヤ, や", "ユ, ゆ", "ヨ, よ","ヤ, や", "ユ, ゆ", "ヨ, よ", "ヤ, や", "ユ, ゆ", "ヨ, よ",    // * 3 (also used as suffixes)
 // 
 
 // 5 lines of the ra/la group: (inclusive) actually spelled with the "r" but pronounced with the "L" sounds -- they too are special -- and also like to go naked
 //  ra        ri         ru         re         ro * 1    
-   "raラ, ら", "riリ, り", "ruル, る", "reレ, れ", "roロ, ろ", 
+   "ラ, ら", "リ, り", "ル, る", "レ, れ", "ロ, ろ", 
 //             ^----^---v
-              "riyaリャ, りゃ", "riyuリュ, りゅ", "riyoリョ, りょ",    "riyaリャ, りゃ", "riyuリュ, りゅ", "riyoリョ, りょ",    // * 2 // ri, ya yu yo 
+              "リャ, りゃ", "リュ, りゅ", "リョ, りょ",    "ャ, りゃ", "リュ, りゅ", "リョ, りょ",    // * 2 // ri, ya yu yo 
 //                      ^              ^              ^             ^               ^             ^ 
 
 // 2 lines (inclusive) of the wa set, plus the nh consonant -- always fully naked AND never have suffixes of ya yu or yo 
 //  wa wo nh * 2
-   "waワ, わ", "woヲ, を",   "nhン, ん",       "waワ, わ", "woヲ, を",   "nhン, ん" }  // * 2 //  and the "}" must be on this line rather than on a line of its own 
+   "ワ, わ", "ヲ, を",   "ン, ん",       "ワ, わ", "ヲ, を",   "ン, ん" }  // * 2 //  and the "}" must be on this line rather than on a line of its own 
 
-
+   
 
 	for {
 		randIndex := rand.Intn(len(keyValuePairs))
@@ -109,7 +109,7 @@ func main() {
 			//fmt.Println("You typed", in, "which is not", value, "so you guessed wrongly")
 			fmt.Printf("\n Oops! it was %s", value)
 
-// hints: 
+			// hints: 
 
 			if value == "ほ" {
 				fmt.Printf(", hint: ring--->, \"-\" \n")
@@ -278,40 +278,5 @@ func main() {
 	}
 }
 /*
-ChatGPT 3.5 had the following observations on the above code that I authored without any of its help:
-
-I have a few suggestions for improving the code's readability and maintainability:
-
-Format the code properly: Indentation and consistent spacing are essential for code readability. Make sure all code blocks are indented correctly, and there 
-are no unnecessary blank lines.
-
-Simplify the hints section: You have duplicate hints for certain characters (e.g., "ほ"), which may lead to confusion if you ever need to modify the hints. 
-Consider using a mapping of characters to hints to avoid redundancy and make the hints section more manageable.
-
-Consider using a struct for key-value pairs: Instead of using a slice of strings for key-value pairs, you could create a custom struct that holds the romaji, 
-katakana, and hiragana characters. This would make the code more structured and allow you to access the components more easily.
-
-Separate the data from the main function: Move the key-value pairs to a separate function or a package-level variable. Keeping the data separate from the main 
-function helps improve code organization and makes it easier to modify or extend the game's data.
-
-Add comments for the hints: While the hints are clear for some characters, they might not be immediately obvious to everyone. Adding comments to explain the 
-logic behind each hint can be helpful for better understanding.
-
-Address the "n" character: As the code stands, there are no hints for the "n" character, which might make it confusing for the user. Consider adding a hint for 
-this character as well.
-
-Overall, your code looks great, and it's a creative way to help users learn katakana and hiragana characters. The suggestions provided above are meant to 
-enhance the code's maintainability and make it even more user-friendly. Keep up the good work!
-
-
-Apologies for the oversight. You are absolutely right. The code on or about line 273 does indeed provide a hint for the "ん" character. 
-It prints the hint ", hint: Y char" when the user is asked to input the hiragana for "ん."
-
-This hint is useful because the romaji for "ん" is "n," and its hiragana is "ん." In the hint, "Y char" likely refers to the keyboard key "Y," which corresponds 
-to the hiragana "ん" when typing in Japanese characters using a standard QWERTY keyboard layout.
-
-So, with that hint, users should be able to recognize that they need to type "Y" on their keyboard to enter the correct hiragana "ん" for the romaji "n."
-
-Once again, I apologize for the oversight, and thank you for pointing that out. Your code looks even more comprehensive now with hints provided for all 
-characters, including "ん." Great job!
+ChatGPT 3.5 proofread all of the katakana, hiragana pairs used in this code; and my Japanese wife verified them also. 
 */
